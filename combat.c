8f53d93a0d29e73ecbc7c5512320919206ae59a6
Jose Yepez <yejo32602@gmail.com>
2026-06-01 19:48:05 +0000

    Fixed combat

--- combat.c
+++ combat.c
@@ -1,16 +1,17 @@
 #include "combat.h"
 #include <stdio.h>
+# include <stdlib.h>
 
-double calculate_damage(int bravery,int attack,int defense){ // Two Parameters, two integers
+double calculate_damage(char *name, int bravery, int attack, int defense){ // Two Parameters, two integers
 
     double adjusted_attack = attack * (1 + (bravery/100.0f));
     printf("Adjusted Damage:%.2lf\n",adjusted_attack);
     double damage = adjusted_attack - defense;
     printf("Damage after defense:%.2lf\n",damage);
-
-
     if (damage < 0) return 0;
     return damage;
-
 }
 
+void heal(double *health, int *potions, const int potion_strength){
+
+}
